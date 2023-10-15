3f07f972be1d94b6fde759b9ff1a181dcbd467f5
Tulio Gomes Braga <129433741+tuliogb@users.noreply.github.com>
2023-10-14 22:43:49 -0300

    ti2022

---
+++ TI/TI1/EstoqueMaster.java/login.js
@@ -0,0 +1,68 @@
+if (localStorage.getItem("usuarios") === null ||  localStorage.getItem("usuarios") === "[]") {
+  localStorage.setItem("usuarios", JSON.stringify([]));
+}
+
+if (localStorage.getItem("logado") === "true") {
+  window.location.href = "produtos.html";
+}
+
+function registrar(event) {
+  event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+
+  var email = document.getElementById("email").value;
+  var senha = document.getElementById("senha").value;
+
+  var usuario = {
+      email: email,
+      senha: senha,
+  };
+
+  var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+  usuarios.push(usuario);
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  alert("Registro concluído com sucesso");
+}
+  
+var btnRegistrar = document.querySelector(".btn-registrar");
+
+if (btnRegistrar){
+  btnRegistrar.addEventListener("click", registrar);
+}
+
+function login(event) {
+  event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+
+  // Obter os valores dos campos de entrada de login
+  var email = document.getElementById("loginEmail").value;
+  var senha = document.getElementById("loginSenha").value;
+
+  // Verificar se as credenciais são válidas
+  var usuarios = JSON.parse(localStorage.getItem("usuarios"));
+  var usuario = usuarios.find(usuario => usuario.email === email);
+  if (usuario === undefined) {
+    alert("Email inválido");
+    return;
+  }
+
+  
+  var storedSenha = usuario.senha;
+
+  if (storedSenha === senha) {
+    alert("Login bem-sucedido");
+    // Redirecionar para a página de estoqur
+    window.location.href = "produtos.html";
+    //Criar variavel global para o sistema reconhecer que está logado:
+    localStorage.setItem("logado", JSON.stringify(true));
+    localStorage.setItem("usuarioLogado", JSON.stringify(usuario));
+  } else {
+    localStorage.setItem("logado", JSON.stringify(false));
+    alert("Credenciais inválidas");
+    // Lidar com credenciais inválidas, como exibir uma mensagem de err
+  }
+}
+
+var btnEntrar = document.querySelector(".btn-entrar");
+if(btnEntrar){
+  btnEntrar.addEventListener("click", login);
+}
+
---
+++ TI/TI1/EstoqueMaster.java/script-config-conta.js
@@ -0,0 +1,108 @@
+if (localStorage.getItem("logado") === null || localStorage.getItem("logado") === "false") {
+    window.location.href = "login.html";
+} else {
+    var usuarioLogado = JSON.parse(localStorage.getItem("usuarioLogado"));
+}
+
+const campoSenha = document.getElementById('campoSenha');
+const campoEmail = document.getElementById('campoEmail');
+
+//Se os inputs acima conterem disabled, serao tornadas textareas:
+if (campoSenha.disabled == true) {
+    campoSenha.type = "text";
+}
+if (campoEmail.disabled == true) {
+    campoEmail.type = "text";
+}
+///Apresenta o email e senha nas text areas
+function apresentaDados(campoSenha, campoEmail) {
+    campoEmail.value = usuarioLogado.email;
+    campoSenha.value = usuarioLogado.senha;
+
+    ///Configura toggle de exibir 
+    var btnToggleSenha = document.getElementById("btnToggleSenha");
+
+    btnToggleSenha.addEventListener("click", function() {
+    if (this.classList.contains("active")) {
+        campoSenha.type = "text";
+    } else {
+        campoSenha.type = "password";
+    }
+    });
+};
+
+apresentaDados(campoSenha, campoEmail);
+
+///Configura botões de editar e salvar
+const btnEditar = document.getElementById("BtnEditar");
+const btnSalvar = document.getElementById("BtnSalvar");
+
+btnEditar.addEventListener("click", () => {
+
+    btnSalvar.hidden = false;
+    btnEditar.hidden = true;
+
+    ///tratamento dos inputs
+    campoEmail.disabled = false;
+    campoSenha.disabled = false;
+    campoSenha.type = "text";
+
+
+})
+
+btnSalvar.addEventListener("click", () => {
+
+    //tratamento botão editar/salvar
+    btnSalvar.hidden = true;
+    btnEditar.hidden = false;
+
+    ///tratamento dos inputs
+    campoEmail.disabled = true;
+    campoSenha.disabled = true;
+    campoSenha.type = "password";
+
+    usuarioLogado.email = campoEmail.value;
+    usuarioLogado.senha = campoSenha.value;
+
+    localStorage.setItem("usuarioLogado", JSON.stringify(usuarioLogado));
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    //ecluir o usuario antigo do localstorage e colocar o usuario logado
+    usuarios.splice(usuarios.indexOf(usuarioLogado), 1, usuarioLogado);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    alert("Dados alterados com sucesso!");
+    
+})
+
+
+//Configura o switch de Notificações
+const switchNotif = document.getElementById("switchNotif");
+const NotifOnOff = document.getElementById("labelNotif");
+
+switchNotif.addEventListener("change", function() {
+    if (this.checked) {
+        console.log("Switch ativado");
+        NotifOnOff.querySelector("h3").textContent = "Notificações";
+    } else {
+        console.log("Switch desativado");
+        NotifOnOff.querySelector("h3").textContent = "Notificações";
+    }
+});
+
+// Configura o botão de dentro do modal
+const btnLimpaDadosModal = document.getElementById("btnLimparDadosModal");
+
+btnLimpaDadosModal.addEventListener("click", () => {
+
+    let confirmaSenha = prompt("Informe sua senha para prosseguir: ");
+
+    if(confirmaSenha == usuarioLogado.senha)
+        localStorage.clear();
+    else alert("Senha incorreta!");
+    window.location.href = "index.html";
+})
+
+var btnLogOut = document.getElementById("btnLogOut");
+btnLogOut.addEventListener("click", () => {
+    localStorage.setItem("logado", JSON.stringify(false));
+    window.location.href = "index.html";
+})  
