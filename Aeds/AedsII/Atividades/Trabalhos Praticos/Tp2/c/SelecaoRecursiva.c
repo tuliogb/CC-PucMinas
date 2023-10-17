b0085c96d66f1a1a052e51d7ae5eda1f564eeddd
Tulio Gomes Braga <129433741+tuliogb@users.noreply.github.com>
2023-10-17 14:51:08 -0300

    .

--- Aeds/AedsII/Atividades/Trabalhos Praticos/Tp2/c/SelecaoRecursiva.c
+++ Aeds/AedsII/Atividades/Trabalhos Praticos/Tp2/c/SelecaoRecursiva.c
@@ -2,14 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 
 /*
     Dev: Tulio Gomes Braga
     Matricula: 1441272
-    Curso: CC-PucMinas - 14/10/2023
+    Curso: CC-PucMinas - 17/10/2023
 */
 
-int tamanho = 0;
+/*
+    InserirElementos: Controla a entrada(stdin) quando ela nao for igual a "FIM" , chamando os outros metodos passando o id e o primeiro endereco vago da lista;
+    SetaId: Abre o arquivo base, ignora a primeira linha, procura a linha do Id digitado, pega a linha e manda pra parteDados;
+    parteDados: Copia a linha e substitui o \n por \0, aloca de tamanho+4 (+4 pra entrar os espacos entre as virgulas) e trata os casos ",,", envia pra SetaDados;
+    setaDados: Parte a String de acordo com ',' e caso a parte for ' ', insere nao informado, se nao, o conteudo dela proprio;
+    mostrarTodos: Percorre a lista enviada ate o tamanho enviado chamando a mostraDados pra printar na formatacao correta;
+    ArqLog: Registra em um arquivo a matricula, tempo de execucao, numero de ocorrencia de comparacao e movimentacao durante o exercicio abaixo;
+
+    Exercicios de Ordenacao: Metodo Selecao Recursiva para ordenar de acordo com o atributo da struct "nome";
+*/
+
+clock_t start,end;
+int tamanho=0,comp=0,mov=0;
 
 typedef struct {
     int id;
@@ -23,7 +36,6 @@ typedef struct {
 } Jogador;
 
 
-
 void InserirElementos(Jogador *lista){
     char input[100];
     scanf("%s", input);
@@ -35,10 +47,9 @@ void InserirElementos(Jogador *lista){
     }
 }
 
-
 void SetaId(Jogador *jogador, int id) {
     FILE *file;
-    file = fopen("players.csv", "r");
+    file = fopen("/tmp/players.csv", "r");
 
     char linha[100];
     char resp[100];
@@ -61,7 +72,6 @@ void SetaId(Jogador *jogador, int id) {
     }
     fclose(file);
 }
-
 
 void parteDados(Jogador *jogador, const char *x) {
 
@@ -96,7 +106,6 @@ void parteDados(Jogador *jogador, const char *x) {
     free(nvlinha);
 }
 
-
 void setaDados(Jogador *jogador,char *linha){
 
     int t=0;
@@ -122,7 +131,6 @@ void setaDados(Jogador *jogador,char *linha){
 
 }
 
-
 void mostraDados(Jogador *jogador){
     printf("[%d ## %s ## %d ## %d ## %d ## %s ## %s ## %s]\n",
         jogador->id,
@@ -136,12 +144,43 @@ void mostraDados(Jogador *jogador){
     );
 }
 
-
 void mostrarTodos(Jogador *lista){
     for(int i=0;i<tamanho;i++){
         mostraDados(&lista[i]);
     }
 }
+
+void troca(Jogador *lista,int x,int y){
+    mov++;
+    Jogador tmp = lista[x];
+    lista[x] = lista[y];
+    lista[y] = tmp;
+}
+
+void ArqLog(){
+    int matricula = 0;
+    int tempo;
+    tempo = (int)(end-start)*1000;
+
+    FILE *file = fopen("matricula_selecaoRecursiva.txt","w");
+    fprintf(file,"%i\t%ims\t%i\t%i",matricula,tempo,comp,mov);
+
+}
+
+void NomeEdata(Jogador *lista,int tam){
+    for (int i=0;i<(tam-1);i++) {
+      int menor=i;
+      for (int j=(i+1);j<tam;j++){
+        comp++;
+         if (lista[menor].anoNascimento == lista[j].anoNascimento){
+             comp++;
+            if(strcmp(lista[menor].nome, lista[j].nome)>0) menor=j;
+         }
+      }
+      troca(lista,menor,i);
+   }
+}
+
 
 
 
@@ -157,18 +196,17 @@ void OrganizaArray(Jogador *lista,int x){
 }
 
 
-void troca(Jogador *lista,int x,int y){
-    Jogador tmp = lista[x];
-    lista[x] = lista[y];
-    lista[y] = tmp;
-}
-
 
 
 int main(){
     Jogador lista[500];
     InserirElementos(lista);
+
+    start=clock();
     OrganizaArray(lista,0);
+    end=clock();
+    ArqLog();
+
     mostrarTodos(lista);
     return 0;
 }
