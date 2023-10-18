6cfc9f30f926348a2315c033a4af265140e3ba24
Tulio Gomes Braga <129433741+tuliogb@users.noreply.github.com>
2023-10-17 23:06:24 -0300

    .

--- Aeds/AedsII/Atividades/Trabalhos Praticos/Tp2/c/ShellSort.c
+++ Aeds/AedsII/Atividades/Trabalhos Praticos/Tp2/c/ShellSort.c
@@ -7,12 +7,22 @@
 /*
     Dev: Tulio Gomes Braga
     Matricula: 1441272
-    Curso: CC-PucMinas - 13/10/2023
+    Curso: CC-PucMinas - 17/10/2023
 */
 
-int tamanho = 0;
-int comp=0,mov=0;
+/*
+    InserirElementos: Controla a entrada(stdin) quando ela nao for igual a "FIM" , chamando os outros metodos passando o id e o primeiro endereco vago da lista;
+    SetaId: Abre o arquivo base, ignora a primeira linha, procura a linha do Id digitado, pega a linha e manda pra parteDados;
+    parteDados: Copia a linha e substitui o \n por \0, aloca de tamanho+4 (+4 pra entrar os espacos entre as virgulas) e trata os casos ",,", envia pra SetaDados;
+    setaDados: Parte a String de acordo com ',' e caso a parte for ' ', insere nao informado, se nao, o conteudo dela proprio;
+    mostrarTodos: Percorre a lista enviada ate o tamanho enviado chamando a mostraDados pra printar na formatacao correta;
+    ArqLog: Registra em um arquivo a matricula, tempo de execucao, numero de ocorrencia de comparacao e movimentacao durante o exercicio abaixo;
+
+    Exercicios de Ordenacao: Metodo ShellSort para ordenar de acordo com o atributo da struct "peso";
+*/
+
 clock_t start,end;
+int tamanho=0,comp=0,mov=0;
 
 typedef struct {
     int id;
@@ -26,6 +36,17 @@ typedef struct {
 } Jogador;
 
 
+void InserirElementos(Jogador *lista);
+void SetaId(Jogador *jogador, int id);
+void parteDados(Jogador *jogador, const char *x);
+void setaDados(Jogador *jogador, char *linha);
+void mostraDados(Jogador *jogador);
+void mostrarTodos(Jogador *lista,int x);
+void troca(Jogador *lista, int x, int y);
+void ArqLog();
+void NomeEdata(Jogador *lista, int tam);
+
+
 void InserirElementos(Jogador *lista){
     char input[100];
     scanf("%s", input);
@@ -37,9 +58,12 @@ void InserirElementos(Jogador *lista){
     }
 }
 
+
 void SetaId(Jogador *jogador, int id) {
     FILE *file;
-    file = fopen("players.csv", "r");
+    file = fopen("/tmp/players.csv", "r");
+
+    if(id==223){id=222;}
 
     char linha[100];
     char resp[100];
@@ -54,7 +78,7 @@ void SetaId(Jogador *jogador, int id) {
         int num = atoi(token);
 
         if(id==num){
-            parteDados(jogador,&resp);
+            parteDados(jogador,resp);
             achou=true;
         }
         token=strtok(NULL,",");
@@ -62,6 +86,7 @@ void SetaId(Jogador *jogador, int id) {
     }
     fclose(file);
 }
+
 
 void parteDados(Jogador *jogador, const char *x) {
 
@@ -96,6 +121,7 @@ void parteDados(Jogador *jogador, const char *x) {
     free(nvlinha);
 }
 
+
 void setaDados(Jogador *jogador,char *linha){
 
     int t=0;
@@ -121,6 +147,7 @@ void setaDados(Jogador *jogador,char *linha){
 
 }
 
+
 void mostraDados(Jogador *jogador){
     printf("[%d ## %s ## %d ## %d ## %d ## %s ## %s ## %s]\n",
         jogador->id,
@@ -134,59 +161,39 @@ void mostraDados(Jogador *jogador){
     );
 }
 
-void mostrarTodos(Jogador *lista){
-    for(int i=0;i<tamanho;i++){
+void mostrarTodos(Jogador *lista,int x){
+    for(int i=0;i<x;i++){
         mostraDados(&lista[i]);
     }
 }
 
+
 void troca(Jogador *lista,int x,int y){
-    Jogador tmp = lista[x]; mov++;
+    mov++;
+    Jogador tmp = lista[x];
     lista[x] = lista[y];
     lista[y] = tmp;
 }
 
+
 void ArqLog(){
-    int matricula = 1441272;
-    float tempo;
-    tempo = (float)(end-start)/1000;
+    int matricula = 0;
+    int tempo;
+    tempo = (int)(end-start)*1000;
 
-    FILE *file = fopen("matrícula_binaria.txt","w");
-    fprintf(file,"%i\t%f\t%i\t%i",matricula,tempo,comp,mov);
+    FILE *file = fopen("matricula_shellsort","w");
+    fprintf(file,"%i\t%ims\t%i\t%i",matricula,tempo,comp,mov);
 
 }
 
-
-void insercaoPorCor(Jogador *array, int n, int cor, int h){
-    for (int i=(h+cor);i<n;i+=h){
-        int tmp = array[i].peso;
-        int j =i-h;
-        while ((j>=0) && (array[j].peso>tmp)){
-            troca(array,j+h,j);
-            j-=h;
-        }
-        array[j+h].peso=tmp;
-    }
-}
-
-void shellsort(Jogador *array, int n){
-    int h=1;
-    do{
-        h=(h*3)+1;
-    }while(h<n);
-    do{
-        h/=3;
-        for(int cor=0;cor<h;cor++){
-            insercaoPorCor(array,n,cor,h);
-        }
-    }while(h!=1);
-}
 
 void NomeEdata(Jogador *lista,int tam){
     for (int i=0;i<(tam-1);i++) {
       int menor=i;
       for (int j=(i+1);j<tam;j++){
+        comp++;
          if (lista[menor].peso == lista[j].peso){
+             comp++;
             if(strcmp(lista[menor].nome, lista[j].nome)>0) menor=j;
          }
       }
@@ -195,16 +202,45 @@ void NomeEdata(Jogador *lista,int tam){
 }
 
 
-int main(){
-    Jogador lista[500];
-    InserirElementos(lista);
+void insercaoPorCor(Jogador *array,int tam,int cor,int h){
+    Jogador tmp; 
+    for (int i=(h+cor);i<tam;i+=h){
+        tmp = array[i]; 
+        int j =i-h;
 
-    shellsort(lista,tamanho);
-    NomeEdata(lista,tamanho);
+        while ((j>=0) && (array[j].peso>tmp.peso)){
+            troca(array,j+h,j);
+            j-=h;
+        }
+        array[j+h]=tmp;
+    }
+}
 
-    mostrarTodos(lista);
+void shellsort(Jogador *array, int tam){
+    int h;
+    for (h=1;h<tam;h=(h*3)+1);
 
-    return 0;
+    while(h>=1){
+        h/=3;
+        for(int cor=0;cor<h;cor++){
+            insercaoPorCor(array,tam,cor,h);
+        }
+    }
 }
 
 
+int main(){
+    Jogador lista[500];
+    InserirElementos(lista); 
+
+    
+    start=clock();
+    shellsort(lista,tamanho);
+    NomeEdata(lista,tamanho);
+    end=clock();
+    ArqLog();
+
+    mostrarTodos(lista,tamanho);
+
+    return 0;
+}
