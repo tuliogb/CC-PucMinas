aca014b3310d06efb34b112b087ff0539cae4beb
Tulio Gomes Braga <129433741+tuliogb@users.noreply.github.com>
2023-10-21 11:31:14 -0300

    arvore

--- Aeds/AedsII/Atividades/Estudos/Java/ArvoreB.java
+++ Aeds/AedsII/Atividades/Estudos/Java/ArvoreB.java
@@ -23,7 +23,7 @@ class Arvore{
         raiz = inserir(x,raiz);
     }   
 
-    Arvore inserir(int x,Arvore i){
+    public Arvore inserir(int x,Arvore i){
         if (i==null){  i = new Arvore(x);  }
         else if (x < i.elemento){  i.esq = inserir(x, i.esq);  }
         else if (x > i.elemento){  i.dir = inserir(x, i.dir);  }
@@ -31,16 +31,36 @@ class Arvore{
         return i;
     }
 
+    public void mostrar(Arvore nodo) {
+        if (nodo != null) {
+            mostrar(nodo.esq); // Recursivamente percorre a subárvore esquerda
+            System.out.print(nodo.elemento + " "); // Exibe o elemento atual
+            mostrar(nodo.dir); // Recursivamente percorre a subárvore direita
+        }
+    }
+    
+    /*
+    public Arvore VerificaNum(int x,Arvore i){
+        if(i.elemento == x){ System.out.println("Elemento Encontrado"); }
+        else if (x < i.elemento){  i.esq = VerificaNum(x, i.esq);  }
+        else if (x > i.elemento){  i.dir = VerificaNum(x, i.dir);  }
+
+        return i;
+    }
+    */
+
 
     public static void main(String[] args) {
         Arvore arvore = new Arvore();
 
-        arvore.inserir(10);
-        arvore.inserir(11);
-        arvore.inserir(12);
+        for(int i=0;i<7;i++){
+            arvore.inserir(i);
+        }
 
+        arvore.mostrar(arvore.raiz);
+
+        //arvore.VerificaNum(7,arvore.raiz);
         //System.out.println(arvore.raiz.elemento);   /* PRIMEIRA INSECAO (PONTEIRO RAIZ SE TORNOU I E APONTOU PRO NOVO E VOLTOU PRA RAIZ COM ELEMENTO INSERIDO*/
-
-        System.out.println(arvore.raiz.dir.dir.elemento); /* A POSICAO DO ULTIMO ELEMENTO É RAIZ*ELEMENTOS-1 (QUANDO ESTAO EM SERIE)*/
+        //System.out.println(arvore.raiz.dir.dir.elemento); /* A POSICAO DO ULTIMO ELEMENTO É RAIZ*ELEMENTOS-1 (QUANDO ESTAO EM SERIE)*/
     }
 }
