6beee1f6939c2b28c70dba2a85f58d574cc8b2ee
Tulio Gomes Braga <129433741+tuliogb@users.noreply.github.com>
2023-11-06 15:57:31 -0300

    matriz

--- Aeds/AedsII/Atividades/Estudos/Java/Matriz.java
+++ Aeds/AedsII/Atividades/Estudos/Java/Matriz.java
@@ -8,13 +8,13 @@ public class Matriz {
     public Matriz (int linha, int coluna){
         this.linha = linha;
         this.coluna = coluna;
-     }
+    }
 
-     public void CriaMatriz(){
+    public void CriaMatriz(){
         inicio = new Celula(0);
 
         for (Celula i=inicio;cont<coluna;i=i.dir,cont++){
-            i.dir = new Celula(cont);
+            i.dir = new Celula(0);
             i.dir.esq = i;
         }
         cont = 1;
@@ -25,32 +25,52 @@ public class Matriz {
             inicio = inicio.inf;                                                /* PERDI O MEU INICIO, MAS (INICIO = INICIO.SUP)*linha-1 ocorrencias RECUPERA */
 
             for (Celula i=inicio;cont<coluna;i=i.dir,cont++){
-                i.dir = new Celula(cont);
+                i.dir = new Celula(0);
                 i.dir.esq = i;
             }
             cont=1;
         }
         for (int i=0;i<linha-1;i++)  inicio = inicio.sup;
-     }
+    }
 
 
-     public void mostraMatriz(){
+    public void mostraMatriz(){
         for (int c=0; c<linha; c++){
             for(Celula i=inicio;i!=null;i=i.dir){
-                MyIO.print(i.elemento + "/");
+                MyIO.print("| " + i.elemento + " |");
             }
             MyIO.print("\n");
-            inicio = inicio.inf;
+            if(inicio.inf != null) inicio = inicio.inf;                            /* NO ULTIMO LACO EU TO FAZENDO, ENTAO TA PONTANDO PRA NULO, TO PERDENDO O INICIO */
         }
-     }
+        for (int i=0;i<linha-1;i++)  inicio = inicio.sup;
+    }
+
+    public void inserirValores(){
+        for (int c=0; c<linha; c++){
+            for(Celula i=inicio;i!=null;i=i.dir){
+                i.elemento = MyIO.readInt();
+            }
+            if(inicio.inf != null) inicio = inicio.inf;                            /* NO ULTIMO LACO EU TO FAZENDO, ENTAO TA PONTANDO PRA NULO, TO PERDENDO O INICIO */
+        }
+        for (int i=0;i<linha-1;i++)  inicio = inicio.sup;
+    }
+
+    public void somaMatriz(Matriz um, Matriz dois){
+        for (int c=0; c<linha; c++){
+            for(Celula i=um.inicio, j=dois.inicio;i!=null && j!=null;i=i.dir, j=j.dir){
+               
+            }
+            if(inicio.inf != null) inicio = inicio.inf;                            /* NO ULTIMO LACO EU TO FAZENDO, ENTAO TA PONTANDO PRA NULO, TO PERDENDO O INICIO */
+        }
+        for (int i=0;i<linha-1;i++)  inicio = inicio.sup;
+    }
 
 
 
     public static void main(String[] args) {
         Matriz matriz = new Matriz(3, 3);
         matriz.CriaMatriz();
+        matriz.inserirValores();
         matriz.mostraMatriz();
-        
     }
 }
-
