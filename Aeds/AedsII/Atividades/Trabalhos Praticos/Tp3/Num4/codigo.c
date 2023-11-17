e02653c474499f10bed4c68ccaf8bdde6470d3ba
Tulio Gomes Braga <129433741+tuliogb@users.noreply.github.com>
2023-11-17 17:12:29 -0300

    100%

--- Aeds/AedsII/Atividades/Trabalhos Praticos/Tp3/Num4/codigo.c
+++ Aeds/AedsII/Atividades/Trabalhos Praticos/Tp3/Num4/codigo.c
@@ -24,13 +24,17 @@ typedef struct {
 } Jogador;  Jogador lista[6];
 
 void inserirElementos();
+void verifica(int id);
 void setaId(Jogador *jogador, int id);
 void parteDados(Jogador *jogador, const char *linha);
 void setaDados(Jogador *jogador,char *linha);
 void mostraDados(Jogador *jogador);
 void mostrarTodos();
 void removerInicioPrint();
-
+void removerInicio();
+void calculaMedia();
+void manipulaElementos();
+void decodifica(char *entrada);
 
 
 void inserirElementos(){
@@ -40,17 +44,29 @@ void inserirElementos(){
     while(strcmp(input, "FIM")!= 0){
         int id = atoi(input);
         if ((ultimo+1)%tam == primeiro){
-            removerInicioPrint(lista);
+            removerInicio(lista);
         }
+
         setaId(&lista[ultimo],id);
         ultimo = ((ultimo+1)%tam);
+        calculaMedia();
+;
         scanf("%s", input);
     }
 }
 
+void verifica(int id){
+    if ((ultimo+1)%tam == primeiro){
+        removerInicio(lista);
+    }
+    setaId(&lista[ultimo],id);
+    ultimo = ((ultimo+1)%tam);
+    calculaMedia();
+}
+
 void setaId(Jogador *jogador, int id) {
     FILE *file;
-    file = fopen("players.csv", "r");
+    file = fopen("/tmp/players.csv", "r");
 
     char linha[100];
     char resp[100];
@@ -111,7 +127,6 @@ void setaDados(Jogador *jogador,char *linha){
     int t=0;
     char *parte[8];
     char *token = strtok(linha, ",");
-    tamanho++;
 
     while (token != NULL && t < 8) {
         parte[t] = token;
@@ -128,14 +143,7 @@ void setaDados(Jogador *jogador,char *linha){
     if(strcmp(parte[6], " ")== 0)strcpy(jogador->cidadeNascimento, "nao informado");else strcpy(jogador->cidadeNascimento, parte[6]);
     if(strcmp(parte[7], " ")== 0)strcpy(jogador->estadoNascimento, "nao informado");else strcpy(jogador->estadoNascimento, parte[7]);
 
-    printf("%i\n", tamanho);
-    float media=0; int soma=0; int i=primeiro;              // NA PRIMEIRA VEZ O PRIMEIRO == ULTIMO AI ESTA QUEBRANDO A MEDIA;
-    while (i!=ultimo){
-        soma += lista[i].altura;
-        i=(i+1)%tam;
-    }
-    media = soma/(float)tamanho;
-    printf("%0.f\n", media);
+    tamanho++;
 }
 
 void mostraDados(Jogador *jogador){
@@ -167,21 +175,58 @@ void removerInicioPrint(){
     }
 }
 
+void removerInicio(){
+    if(primeiro==ultimo) printf("Erro\n");
+    else{
+        primeiro = (primeiro+1)%tam;
+        tamanho--;
+    }
+}
 
+void calculaMedia(){
+    float media=0;
 
-int main(){
-    inserirElementos();
-    //mostrarTodos();
-    return 0;
+    for(int i=primeiro; i!=ultimo; i=(i+1)%tam){
+        media = media + lista[i].altura;
+    }
+
+    printf("%0.f\n", media/tamanho);
+}
+
+void manipulaElementos(){
+    int qtd; scanf("%i",&qtd);
+    char entrada[20];
+    print = true;
+
+    while (getchar() != '\n');
+
+    for (int i=0;i<qtd;i++){
+        fgets(entrada, sizeof(entrada), stdin);                   
+        decodifica(entrada);
+    }
+}
+
+void decodifica(char *entrada){
+    int len = strlen(entrada);
+    if (len>0 && entrada[len-1]=='\n'){entrada[len-1]='\0';}
+    char *token = strtok(entrada," ");
+
+    if(token!=NULL){
+        if (strcmp(token, "I")==0){
+            token = strtok(NULL, " ");
+            verifica(atoi(token));
+        }
+        else if(strcmp(entrada, "R")==0){
+            removerInicioPrint();
+        }
+    }
 }
 
 
 
-
-
-
-
-
-
-
-
+int main(){
+    inserirElementos();
+    manipulaElementos();
+    mostrarTodos();
+    return 0;
+}
