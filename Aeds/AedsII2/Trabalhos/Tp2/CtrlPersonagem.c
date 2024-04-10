b4a64a51b2ca167f21d0746e8839072b6d8d7871
Túlio Braga <129433741+tuliogb@users.noreply.github.com>
2024-04-10 18:48:20 -0300

    v1

---
+++ Aeds/AedsII2/Trabalhos/Tp2/CtrlPersonagem.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAXTAM 405
+
+typedef struct{
+    int dia, mes, ano;
+} LocalDate;
+
+typedef struct {
+    char id[50], name[50], alternate_names[200], house[50], ancestry[50], species[50], patronus[50], hogwartsStaff[50], hogwartsStudent[50], actorName[50], alternate_actors[100], eyeColour[50], gender[50], hairColour[50];
+    LocalDate dateOfBirth, yearOfBirth;
+    bool alive, wizard; 
+} Personagem;
+
+Personagem Base[MAXTAM];
+int baseTam = 0, entradas = 0;
+Personagem Lista[MAXTAM];
+
+
+void mostraPersonagem(Personagem p){
+    printf("[%s ## %s ## {%s} ## %s ## %s ## %s ## %s ## %s ## %s ## %s ## %s ## %02i-%02i-%i ## %i ## %s ## %s ## %s ## %s]\n" , 
+        p.id, 
+        p.name, 
+        p.alternate_names,
+        p.house,
+        p.ancestry,
+        p.species,
+        p.patronus,
+        p.hogwartsStaff,
+        p.hogwartsStudent,
+        p.actorName,
+        p.alive ? "true" : "false",
+        p.dateOfBirth.dia, p.dateOfBirth.mes, p.dateOfBirth.ano,
+        p.yearOfBirth.ano,
+        p.eyeColour,
+        p.gender,
+        p.hairColour,
+        p.wizard ? "true" : "false"
+    );
+}
+
+void mostrarBase(){
+    for(int i=0; i<baseTam; i++){
+        mostraPersonagem(Base[i]);
+    }
+}
+
+void setaLocalDate(Personagem *p, char data[]){ 
+    char dia[3], mes[3], ano[5];
+
+    strncpy(dia, data, 2);
+    dia[2] = '\0'; 
+    p->dateOfBirth.dia = atoi(dia);
+
+    if(data[4]!='-'){   strncpy(mes, data+3, 2);    mes[2]='\0';    p->dateOfBirth.mes = atoi(mes);}
+    else{ strncpy(mes, data+3, 1);    mes[1]='\0';    p->dateOfBirth.mes = atoi(mes);}
+
+    if(data[4]!='-'){   strncpy(ano, data+6, 4);    ano[4]='\0';    p->dateOfBirth.ano = atoi(ano);}
+    else{ strncpy(ano, data+5, 4);    ano[4]='\0';    p->yearOfBirth.ano = p->dateOfBirth.ano = atoi(ano);}
+}
+
+void setaPersonagem(char* linha){
+    char input[300], data[11]; strcpy(input,linha);
+    int x=0, y=0, l=0;
+
+    while(input[x]!=';'){ Base[baseTam].id[y] = input[x]; x++; y++;}    Base[baseTam].id[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].name[y] = input[x]; x++; y++;}  Base[baseTam].name[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ if(input[x]!='[' && input[x]!=']' && input[x]!=39){ Base[baseTam].alternate_names[y] = input[x]; y++;} x++;}  Base[baseTam].alternate_names[y]='\0'; x++; y=0;   
+    while(input[x]!=';'){ Base[baseTam].house[y] = input[x]; x++; y++;} Base[baseTam].house[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].ancestry[y] = input[x]; x++; y++;}  Base[baseTam].ancestry[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].species[y] = input[x]; x++; y++;}   Base[baseTam].species[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].patronus[y] = input[x]; x++; y++;}  Base[baseTam].patronus[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].hogwartsStaff[y] = input[x]; x++; y++;} Base[baseTam].hogwartsStaff[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].hogwartsStudent[y] = input[x]; x++; y++;}   Base[baseTam].hogwartsStudent[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].actorName[y] = input[x]; x++; y++;}  Base[baseTam].actorName[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ if(input[x]=='V'){ Base[baseTam].alive=true; x+=10;} else{ Base[baseTam].alive=false; x+=5;}} x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].alternate_actors[y] = input[x]; x++; y++;}  Base[baseTam].alternate_actors[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ data[y] = input[x]; x++; y++;} setaLocalDate(&Base[baseTam],data); x++; y=0;
+    while(input[x]!=';'){ x++; y++;}  x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].eyeColour[y] = input[x]; x++; y++;}  Base[baseTam].eyeColour[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].gender[y] = input[x]; x++; y++;}  Base[baseTam].gender[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ Base[baseTam].hairColour[y] = input[x]; x++; y++;}  Base[baseTam].hairColour[y]='\0'; x++; y=0;
+    while(input[x]!=';'){ if(input[x]=='V'){ Base[baseTam].wizard=true; x+=9;} else{ Base[baseTam].wizard=false; x+=4;}}
+
+    baseTam++;
+}
+
+void setaBase(){
+    FILE *file =  fopen("characters.csv","r");
+    char input[300];
+    fgets(input, sizeof(input), file);
+    
+    while(fgets(input, sizeof(input), file) != NULL){
+        setaPersonagem(input);
+    }
+}
+
+
+int main(){
+    setaBase();
+    mostrarBase();
+}
--- Aeds/AedsII2/Trabalhos/Tp2/CtrlPersonagem.java
+++ Aeds/AedsII2/Trabalhos/Tp2/CtrlPersonagem.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.time.LocalDate;
@@ -12,87 +13,6 @@ class Personagem {
     private LocalDate dateOfBirth;
     private int yearOfBirth;
     private boolean wizard, alive;
-
-
-    public void setId(String id) {this.id = id;}
-    public String getId() {return id;}
-
-    public void setName(String name) {this.name = name;}
-    public String getName() {return name;}
-
-    public void setHouse(String house) {this.house = house;}
-    public String getHouse() {return house;}
-
-    public void setAncestry(String ancestry) {this.ancestry = ancestry;}
-    public String getAncestry() {return ancestry;}
-
-    public void setSpecies(String species) {this.species = species;}
-    public String getSpecies() {return species;}
-
-    public void setPatronus(String patronus) {this.patronus = patronus;}
-    public String getPatronus() {return patronus;}
-
-    public void setHogwartsStaff(String hogwartsStaff) {this.hogwartsStaff = hogwartsStaff;}
-    public String getHogwartsStaff() {return hogwartsStaff;}
-
-    public void setHogwartsStudent(String hogwartsStudent) {this.hogwartsStudent = hogwartsStudent;}
-    public String getHogwartsStudent() {return hogwartsStudent;}
-
-    public void setActorName(String actorName) {this.actorName = actorName;}
-    public String getActorName() {return actorName;}
-
-    public void setEyeColour(String eyeColour) {this.eyeColour = eyeColour;}
-    public String getEyeColour() {return eyeColour;}
-
-    public void setGender(String gender) {this.gender = gender;}
-    public String getGender() {return gender;}
-
-    public void setHairColour(String hairColour) {this.hairColour = hairColour;}
-    public String getHairColour() {return hairColour;}
-
-    public void setYearOfBirth(String yearOfBirth) { if(!yearOfBirth.equals("") && !yearOfBirth.equals("yearOfBirth")) this.yearOfBirth = Integer.parseInt(yearOfBirth);}
-    public int getYearOfBirth() {return yearOfBirth;}
-
-    public void setAlive(String alive) {boolean x = false;  if(alive.equals("true")) x=true;    this.alive = x;}
-    public boolean getAlive() {return alive;}
-
-    public void setWizard(String wizard) {boolean x = false; if(wizard.equals("true")) x=true;  this.wizard = x;}
-    public boolean getWizard() {return wizard;}
-
-    public void setDateOfBirth(String dateOfBirth) { 
-        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd-M-yyyy"); 
-        DateTimeFormatter format2 = DateTimeFormatter.ofPattern("dd-MM-yyyy"); 
-
-        if(!dateOfBirth.equals("") && !dateOfBirth.equals("dateOfBirth") && !dateOfBirth.equals("[]")) {
-            if(dateOfBirth.charAt(2)=='-' && dateOfBirth.charAt(4)!='-') this.dateOfBirth = LocalDate.parse(dateOfBirth, format2);
-            else this.dateOfBirth = LocalDate.parse(dateOfBirth, format1);
-        }
-    }
-    public LocalDate getDateOfBirth() {return dateOfBirth;}
-
-    public void setAlternateNames(String alternate_names) {
-        if(!alternate_names.equals("[]")){
-            alternate_names = alternate_names.replaceAll("['\"\\[\\]]", "");
-            String[] parte = alternate_names.split(";");
-
-            for(int i=0; i<parte.length; i++){
-                this.alternate_names[i] = parte[i];
-            }
-        }
-    }
-    public String[] getAlternateNames() {return alternate_names;}
-
-    public void setAlternateActors(String alternate_actors) {
-        if(!alternate_actors.equals("[]")){
-            alternate_actors = alternate_actors.replaceAll("['\"\\[\\]]", "");
-            String[] parte = alternate_actors.split(";");
-
-            for(int i=0; i<parte.length; i++){
-                this.alternate_actors[i] = parte[i];
-            }
-        }
-    }
-    public String[] getAlternateActors() {return alternate_actors;}
 
 
     public Personagem() {
@@ -116,9 +36,9 @@ class Personagem {
         this.alive = false;
     }
 
-    public Personagem(String id, String nome, String house, String ancestry, String species, String patronus, String hogwartsStaff, String hogwartsStudent, String actorName, String eyeColour, String gender, String hairColour, String[] alternate_names, String[] alternate_actors, LocalDate dateOfBirth, int yearOfBirth, boolean alive, boolean wizard) {
+    public Personagem(String id, String name, String house, String ancestry, String species, String patronus, String hogwartsStaff, String hogwartsStudent, String actorName, String eyeColour, String gender, String hairColour, String[] alternate_names, String[] alternate_actors, LocalDate dateOfBirth, int yearOfBirth, boolean alive, boolean wizard) {
         this.id = id;
-        this.name = nome;
+        this.name = name;
         this.house = house;
         this.ancestry = ancestry;
         this.species = species;
@@ -137,30 +57,132 @@ class Personagem {
         this.wizard = wizard;
     }
 
+    public void setId(String id) {this.id = id;}
+    public String getId() {return id;}
+
+    public void setName(String name) {this.name = name;}
+    public String getName() {return name;}
+
+    public void setHouse(String house) {this.house = house;}
+    public String getHouse() {return house;}
+
+    public void setAncestry(String ancestry) {this.ancestry = ancestry;}
+    public String getAncestry() {return ancestry;}
+
+    public void setSpecies(String species) {this.species = species;}
+    public String getSpecies() {return species;}
+
+    public void setPatronus(String patronus) {this.patronus = patronus;}
+    public String getPatronus() {return patronus;}
+
+    public void setHogwartsStaff(String hogwartsStaff) {this.hogwartsStaff = hogwartsStaff;}
+    public String getHogwartsStaff() {return hogwartsStaff.equals("VERDADEIRO") ? "true" : "false";}
+
+    public void setHogwartsStudent(String hogwartsStudent) {this.hogwartsStudent = hogwartsStudent;}
+    public String getHogwartsStudent() {return hogwartsStudent.equals("VERDADEIRO") ? "true" : "false";}
+
+    public void setActorName(String actorName) {this.actorName = actorName;}
+    public String getActorName() {return actorName;}
+
+    public void setEyeColour(String eyeColour) {this.eyeColour = eyeColour;}
+    public String getEyeColour() {return eyeColour;}
+
+    public void setGender(String gender) {this.gender = gender;}
+    public String getGender() {return gender;}
+
+    public void setHairColour(String hairColour) {this.hairColour = hairColour;}
+    public String getHairColour() {return hairColour;}
+
+    public void setYearOfBirth(String yearOfBirth) { if(!yearOfBirth.equals("") && !yearOfBirth.equals("yearOfBirth")) this.yearOfBirth = Integer.parseInt(yearOfBirth);}
+    public int getYearOfBirth() {return yearOfBirth;}
+
+    public void setAlive(String alive) {boolean x = false;  if(alive.equals("VERDADEIRO")) x=true;    this.alive = x;}
+    public boolean getAlive() {return alive;}
+
+    public void setWizard(String wizard) {boolean x = false; if(wizard.equals("VERDADEIRO")) x=true;  this.wizard = x;}
+    public boolean getWizard() {return wizard;}
+
+    public void setDateOfBirth(String dateOfBirth) { 
+        if(!dateOfBirth.equals("") && !dateOfBirth.equals("dateOfBirth") && !dateOfBirth.equals("[]")){
+            String[] parte = dateOfBirth.split("-");
+            this.dateOfBirth = LocalDate.of(Integer.parseInt(parte[2]), Integer.parseInt(parte[1]), Integer.parseInt(parte[0])); 
+        }
+    }
+    public String getDateOfBirth() { DateTimeFormatter f = DateTimeFormatter.ofPattern("dd-MM-yyyy"); String dt = this.dateOfBirth.format(f); return dt;}
+
+    public void setAlternateNames(String alternate_names) {
+        if(!alternate_names.equals("[]")){
+            alternate_names = alternate_names.replaceAll("[\\'\\[\\]]", ""); //replaceAll("[\\s'\\[\\]]", "");
+            String[] parte = alternate_names.split(",");
+
+            for(int i=0; i<parte.length; i++){
+                this.alternate_names[i] = parte[i]; System.out.print("");
+            }
+        }
+    }
+    public String[] getAlternateNames() {return alternate_names;}
+
+    public void setAlternateActors(String alternate_actors) {
+        if(!alternate_actors.equals("[]")){
+            alternate_actors = alternate_actors.replaceAll("[\\s'\\[\\]]", "");
+            String[] parte = alternate_actors.split(",");
+
+            for(int i=0; i<parte.length; i++){
+                this.alternate_actors[i] = parte[i];
+            }
+        }
+    }
+    public String[] getAlternateActors() { return alternate_actors;}
+
+    public void clone(Personagem p) {
+        this.id = p.id;
+        this.name = p.name;
+        this.house = p.house;
+        this.ancestry = p.ancestry;
+        this.species = p.species;
+        this.patronus = p.patronus;
+        this.hogwartsStaff = p.hogwartsStaff;
+        this.hogwartsStudent = p.hogwartsStudent;
+        this.actorName = p.actorName;
+        this.eyeColour = p.eyeColour;
+        this.gender = p.gender;
+        this.hairColour = p.hairColour;
+        this.alternate_names = p.alternate_names;
+        this.alternate_actors = p.alternate_actors;
+        this.dateOfBirth = p.dateOfBirth;
+        this.yearOfBirth = p.yearOfBirth;
+        this.alive = p.alive;
+        this.wizard = p.wizard;
+    }
+
     public void mostrarPersonagem(){
-        System.out.print(this.id +" "+ this.name +" ");
+        System.out.print("["+ this.id +" ## "+ this.name +" ## {");
 
         for(int i=0; i<this.alternate_names.length; i++){
-            if(this.alternate_names[i]!=null) System.out.print(this.alternate_names[i] +" ");
+            if(this.alternate_names[i]!=null) System.out.print(this.alternate_names[i]);
             else i=this.alternate_names.length;
-        }    
+            if(i<10 && this.alternate_names[i+1]!=null) System.out.print(","); 
+        }  
 
-        System.out.print(this.house +" "+ this.ancestry +" "+ this.species +" "+ this.patronus +" "+ this.hogwartsStaff +" "+ this.hogwartsStudent +" "+ this.alive +" ");
-        
+        System.out.print("} ## " + this.house +" ## "+ this.ancestry +" ## "+ this.species +" ## "+ this.patronus +" ## "+ this.getHogwartsStaff() +" ## "+ this.getHogwartsStudent() +" ## "+ this.actorName +" ## "+ this.alive +" ## ");
+        /*
         for(int i=0; i<this.alternate_actors.length; i++){
-            if(this.alternate_actors[i]!=null) System.out.print(this.alternate_actors[i] +" ");
+            if(this.alternate_actors[i]!=null) System.out.print(this.alternate_actors[i] +" ## ");
             else i=this.alternate_names.length;
         }    
-
-        System.out.println(this.dateOfBirth +" "+ this.yearOfBirth +" "+ this.eyeColour +" "+ this.gender +" "+ this.hairColour +" "+ this.wizard);
+        */ 
+        System.out.println(this.getDateOfBirth() +" ## "+ this.yearOfBirth +" ## "+ this.eyeColour +" ## "+ this.gender +" ## "+ this.hairColour +" ## "+ this.wizard +"]");
     }
+
+    public void lerPersonagem(){}
+    
 }
 
 public class CtrlPersonagem{
 
-    static int tam = 0;
-    static int maxTam = 406;
+    static int tamBase=0, entradas=0, maxTam=405;;
     static Personagem[] Base =  new Personagem[maxTam];
+    static Personagem[] Lista = new Personagem[maxTam];
 
 
     static void setaBase() throws Exception{
@@ -169,31 +191,15 @@ public class CtrlPersonagem{
         BufferedReader bf = new BufferedReader(file);
 
         while((linha=bf.readLine()) != null){
-            setaPersonagem(preparaLinha(linha));
+            setaPersonagem(linha);
         }
 
         bf.close();
         file.close();
     }
 
-    static String preparaLinha(String linha){
-        boolean start = false;
-        char[] array = linha.toCharArray();
-
-         for(int i=0; i<array.length-1; i++){
-            if(array[i]=='"' && array[i+1]=='[') start = true;
-            if(start){
-                if(array[i]==',') array[i]=';';
-            }
-            if(array[i]==']' && array[i+1]=='"') start = false;
-        }
-
-        linha = new String (array);  
-        return linha;
-    }
-
     static void setaPersonagem(String linha){
-        String[] parte = linha.split(",");
+        String[] parte = linha.split(";");
         Personagem p = new Personagem();
 
         p.setId(parte[0]);
@@ -215,24 +221,49 @@ public class CtrlPersonagem{
         p.setHairColour(parte[16]);
         p.setWizard(parte[17]);
 
-        Base[tam++]=p;
+        Base[tamBase++]=p;
     }
 
     static void mostrarBase(){
-        for (int i=0; i<tam; i++){
+        for (int i=0; i<tamBase; i++){
             Base[i].mostrarPersonagem();
         }
     }
 
+    static void setaLista(){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        while(!input.equals("FIM")){
+            setaInput(input);
+            input = sc.nextLine();
+        }
+    }
+
+    static void setaInput(String id){
+        for(int i=0; i<tamBase; i++){
+            if(Base[i].getId().equals(id)){
+                Lista[entradas++] = Base[i];
+                i=tamBase;
+            }
+        }
+    }
+
+    static void mostrarLista(){
+        for (int i=0; i<entradas; i++){
+            Lista[i].mostrarPersonagem();
+        }
+    }
 
     public static void main(String[] args){
         try {
             setaBase();
-            mostrarBase();
+            setaLista();
+            //mostrarBase();
+            mostrarLista();
         } catch(Exception e){ System.out.println(e);}
     }
 }
-
 
 
 
