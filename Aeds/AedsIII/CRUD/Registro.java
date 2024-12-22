b2f88f1c2dd1fa87c9f76576f34860dbc48b87b9
Túlio Braga <129433741+tuliogb@users.noreply.github.com>
2024-12-22 20:41:42 -0300

    at

--- Aeds/AedsIII/CRUD/Registro.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public interface Registro {
-    public void setId(int id);
-    public int getId();
-    public byte[] toByteArray() throws IOException;
-    public void fromByteArray(byte[] b) throws IOException;
-}
