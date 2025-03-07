1eeb1de1362d8a3b6a3f9c409e85b7a4af4a9941
Túlio Braga <129433741+tuliogb@users.noreply.github.com>
2025-03-07 10:02:26 -0300

    v2

--- Grafos/alg1.c
+++ Grafos/alg1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
 int matriz[6][6] = {
 //   a b c d e f
     {0,1,0,1,1,0},  //a
@@ -11,12 +12,40 @@ int matriz[6][6] = {
     {0,0,1,1,1,0}   //f
 };
 
+
+/*
+int matriz[4][4] = {
+    {0, 1, 1, 1},  // vértice A
+    {1, 0, 1, 1},  // vértice B
+    {1, 1, 0, 1},  // vértice C
+    {1, 1, 1, 0}   // vértice D
+};
+*/
+
+
 int visitados[6];
-int ciclos=0, maxLin=5, maxCol=5, visita=0;
+int ciclos=0, maxLin=6, maxCol=6, qtdVisitados=0;
+
+
 
 bool possoIr(int linha)
 {
-    return (linha > visitados[visita]) ? true : false;
+    if(visitados[linha] != 1)   // se eu nao tiver visitado
+    {
+        visitados[linha] = 1;
+        qtdVisitados++;
+        return true;
+    }
+    return false;
+}
+
+void verificaCiclo(int linha, int procurado)
+{
+    if(matriz[linha][procurado]==1 && qtdVisitados>2)
+    {
+        printf("Cliclo marcado\n");
+        ciclos++;
+    } 
 }
 
 void gerarCiclos(int lin, int col, int procurado)
@@ -25,17 +54,47 @@ void gerarCiclos(int lin, int col, int procurado)
     {
         if(matriz[lin][col]==1)
         {
-            visitados[visita]=col;
-            if(possoIr(col)) gerarCiclos(col,0,procurado);
-            else gerarCiclos(lin,col+1,procurado);
+            if(possoIr(col))
+            { 
+                printf("[%i][%i]\n",lin,col);
+                verificaCiclo(lin, procurado);
+                gerarCiclos(col,0,procurado);
+                visitados[col]=0;
+                qtdVisitados--;
+            }
+            gerarCiclos(lin,col+1,procurado);
         }
         else gerarCiclos(lin,col+1,procurado);
     }
 }
 
-int main()
+void removeVertice(int vertice)
 {
-    gerarCiclos(0,0,0);
+    for (int i=0; i<maxLin; i++)
+    { 
+        matriz[i][vertice]=0;
+        matriz[vertice][i]=0;
+    }
 }
 
-//printf("[%i][%i]\n",lin,col);
+void setaVisitados(int i)
+{
+    for (int i=0; i<maxCol; i++) visitados[i]=0;
+    visitados[i] = 1;
+    qtdVisitados++;
+}
+
+
+
+int main()
+{
+    for (int i=0; i<maxLin; i++)
+    {
+        setaVisitados(i);
+        gerarCiclos(i,i,i);
+        printf("\n");
+        removeVertice(i);
+    }
+    printf("Total de ciclos encontrados: %d\n", ciclos/2);
+    return 0;
+}
